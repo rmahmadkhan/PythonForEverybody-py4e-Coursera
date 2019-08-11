8e09d66b0849b8eadbd8c2f623c0a488d571e433
Ahmad Khan <46877003+rmahmadkhan@users.noreply.github.com>
2019-08-11 14:27:06 +0500

    Create week4_assignment_2.2.py

---
+++ Programming for Everybody (Getting Started with Python)/week4_assignment_2.2.py
@@ -0,0 +1,5 @@
+# 2.2 Write a program that uses input to prompt a user for their name and then welcomes them.
+# Note that input will pop up a dialog box.
+# Enter Sarah in the pop-up box when yoname = input("Enter your name")u are prompted so your output will match the desired output.
+
+print("Hello",name)
